4cf04362bdaaf8a39bfabca2c9e207f291719713
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-04-17 16:36:01 -0400

    update index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 
 const usersCtrl = require('./controllers/users');
+const postsCtrl = require('./controllers/posts');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +14,14 @@ app
     .use(express.static('./docs'))
 
     .use('/users', usersCtrl)
+    .use('/posts', postsCtrl)
+
+
+    // All the way at the end of the pipeline. Return instead of not found
+    .get('*', (req, res) => {
+      res.sendFile( path.join(__dirname, '../docs/index.html' ) );
+    })
+    
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
