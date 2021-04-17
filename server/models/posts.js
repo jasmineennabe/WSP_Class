6824fac264f88347468ec1b9e3664c521c6c1ac7
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-04-17 19:00:02 -0400

    personalization

--- server/models/posts.js
+++ server/models/posts.js
@@ -9,6 +9,7 @@ const list = [
         caption: "I love kittens!",
         time: Date(),
         user_handle: "@jasmineennabe",
+        isPublic: true,
     },
     {
         src: "https://www.irreverentgent.com/wp-content/uploads/2016/04/How-to-Workout-Plan-banner-new-1024x683.jpg",
@@ -16,6 +17,7 @@ const list = [
         caption: "No caption",
         time: Date(),
         user_handle: "@johndoe",
+        isPublic: true,
     },
     {
     src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/download-3-1585685825.jpeg",
@@ -23,6 +25,7 @@ const list = [
         caption: "Got my nails done :)",
         time: Date(),
         user_handle: "@janedoe",
+        isPublic: true,
     },
     {
     src: "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg&w=1440",
@@ -30,29 +33,45 @@ const list = [
         caption: "Look at my happy guy",
         time: Date(),
         user_handle: "@jackdoe",
+        isPublic: true,
     },
 ];
 
-module.exports.GetAll = ()=> list.map((x, i) => ({ 
-    ...x, 
-    user: users.GetByHandle(x.user_handle) 
-}) );
-module.exports.Get = (post_id)=> list[post_id];
-module.exports.Add = (post)=> {
-    if(!post.user_handle){
-        throw {code: 422, msg: "Post must have an Owner"}
+    const listWithOwner = ()=> list.map((x, i) => ({ 
+        ...x, 
+        user: users.GetByHandle(x.user_handle) 
+    }) );
+    
+    module.exports.GetAll = ()=> {
+        return listWithOwner();
     }
-     list.push(post);
-     return { ...post };
-}
-module.exports.Update = (post_id, post)=> {
-    const oldObj = list[post_id];
-    const newObj = { ...oldObj, ...post }
-    list[post_id] = newObj ;
-    return newObj;
-}
-module.exports.Delete = (post_id)=> {
-    const post = list[post_id];
-    list.splice(post_id, 1);
-    return post;
-} 
+
+    module.exports.GetWall = (handle)=> {
+        return listWithOwner().filter(post=> post.user_handle == handle);
+    };
+
+    module.exports.GetFeed = (handle)=> listWithOwner()
+        .filter(post=> users.GetByHandle(handle).following.some(f=> f.handle == post.user_handle && f.isApproved) );
+
+    module.exports.Get = (post_id)=> list[post_id];
+
+    module.exports.Add = (post)=> {
+        if(!post.user_handle){
+            throw {code: 422, msg: "Post must have an Owner"}
+        }
+        list.push(post);
+        return { ...post };
+    }
+
+    module.exports.Update = (post_id, post)=> {
+        const oldObj = list[post_id];
+        const newObj = { ...oldObj, ...post }
+        list[post_id] = newObj ;
+        return newObj;
+    }
+
+    module.exports.Delete = (post_id)=> {
+        const post = list[post_id];
+        list.splice(post_id, 1);
+        return post;
+    } 
--- server/models/users.js
+++ server/models/users.js
@@ -13,6 +13,8 @@ const list = [
         handle: '@jasmineennabe',
         pic: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
         password: 'mysecretpassword',
+        isAdmin: true,
+        following: [ { handle: '@johndoe', isApproved: true }, { handle: '@janedoe', isApproved: true }, { handle: '@jackdoe', isApproved: true } ],
     },
     { 
         firstName: 'John',
@@ -20,6 +22,8 @@ const list = [
         handle: '@johndoe',
         pic: "https://randomuser.me/api/portraits/men/32.jpg",
         password: 'imjohndoe',
+        isAdmin: false,
+        following: [ { handle: '@jasmineennabe', isApproved: true }, { handle: '@janedoe', isApproved: true }, { handle: '@jackdoe', isApproved: true } ],
     },
     { 
         firstName: 'Jane',
@@ -27,6 +31,8 @@ const list = [
         handle: '@janedoe',
         pic: "https://randomuser.me/api/portraits/women/67.jpg",
         password: 'imjanedoe',
+        isAdmin: false,
+        following: [ { handle: '@johndoe', isApproved: true }, { handle: '@jasmineennabe', isApproved: true }, { handle: '@jackdoe', isApproved: true } ],
     },
     { 
         firstName: 'Jack',
@@ -34,78 +40,80 @@ const list = [
         handle: '@jackdoe',
         pic: "https://randomuser.me/api/portraits/men/1.jpg",
         password: 'imjackdoe',
+        isAdmin: false,
+        following: [ { handle: '@johndoe', isApproved: true }, { handle: '@janedoe', isApproved: true }, { handle: '@jasmineennabe', isApproved: true } ],
     },
 ];
 
-module.exports.GetAll = ()=> list;
-module.exports.Get = (user_id)=> list[user_id];
-module.exports.GetByHandle = (handle)=> ({ ...list.find( (x, i)=> x.handle == handle ), password: undefined }) ;
-module.exports.Add = (user)=> {
-    if(!user.firstName){
-        throw { code: 422, msg: "First Name is required" }
+    module.exports.GetAll = ()=> list;
+    module.exports.Get = (user_id)=> list[user_id];
+    module.exports.GetByHandle = (handle)=> ({ ...list.find( (x, i)=> x.handle == handle ), password: undefined }) ;
+    module.exports.Add = (user)=> {
+        if(!user.firstName){
+            throw { code: 422, msg: "First Name is required" }
+        }
+        list.push(user);
+        return { ...user, password: undefined };
     }
-     list.push(user);
-     return { ...user, password: undefined };
-}
-module.exports.Register = async (user)=> {
+    module.exports.Register = async (user)=> {
 
-    const hash = await bcrypt.hash(user.password, +SALT_ROUNDS);
+        const hash = await bcrypt.hash(user.password, +SALT_ROUNDS);
 
-    user.password = hash;
+        user.password = hash;
 
-    if(!user.firstName){
-        throw { code: 422, msg: "First Name is required" }
+        if(!user.firstName){
+            throw { code: 422, msg: "First Name is required" }
+        }
+
+        list.push(user);
+        return { ...user, password: undefined };
+
     }
-
-    list.push(user);
-    return { ...user, password: undefined };
-
-}
-module.exports.Update = (user_id, user)=> {
-    const oldObj = list[user_id];
-    if(user.firstName){
-        oldObj.firstName = user.firstName;
+    module.exports.Update = (user_id, user)=> {
+        const oldObj = list[user_id];
+        if(user.firstName){
+            oldObj.firstName = user.firstName;
+        }
+        if(user.lastName){
+            oldObj.lastName = user.lastName;
+        }
+        if(user.handle){
+            oldObj.handle = user.handle;
+        }
+        if(user.pic){
+            oldObj.pic = user.pic;
+        }
+        //list[user_id] = newObj ;
+        return { ...oldObj, password: undefined };
     }
-    if(user.lastName){
-        oldObj.lastName = user.lastName;
+    module.exports.Delete = (user_id)=> {
+        const user = list[user_id];
+        list.splice(user_id, 1);
+        return user;
     }
-    if(user.handle){
-        oldObj.handle = user.handle;
-    }
-    if(user.pic){
-        oldObj.pic = user.pic;
-    }
-    //list[user_id] = newObj ;
-    return { ...oldObj, password: undefined };
-}
-module.exports.Delete = (user_id)=> {
-    const user = list[user_id];
-    list.splice(user_id, 1);
-    return user;
-}
-module.exports.Login = async (handle, password) =>{
-    console.log({ handle, password})
-    const user = list.find(x=> x.handle == handle && x.password == password);
-    if(!user) throw { code: 401, msg: "Wrong Username or Password" };
-    const user = list.find(x=> x.handle == handle);
-    if(!user) throw { code: 401, msg: "Sorry there is no user with that handle" };
+    module.exports.Login = async (handle, password) =>{
+        console.log({ handle, password})
+        const user = list.find(x=> x.handle == handle && x.password == password);
+        if(!user) throw { code: 401, msg: "Wrong Username or Password" };
+        const user = list.find(x=> x.handle == handle);
+        if(!user) throw { code: 401, msg: "Sorry there is no user with that handle" };
 
-    if( ! await bcrypt.compare(password, user.password) ){
-        throw { code: 401, msg: "Wrong Password" };
+        if( ! await bcrypt.compare(password, user.password) ){
+            throw { code: 401, msg: "Wrong Password" };
+        }
+
+        const data = { ...user, password: undefined };
+
+        const token = jwt.sign(data, JWT_SECRET)
+
+        return { user: data, token };
     }
-
-    const data = { ...user, password: undefined };
-
-    const token = jwt.sign(data, JWT_SECRET)
-
-    return { user, token };
-}
-module.exports.FromJWT = async (token) =>{
-    try {
-        const user = jwt.verify(token, JWT_SECRET);
-        return user;       
-    } catch (error) {
-        console.log({error});
-        return null;
+    module.exports.FromJWT = async (token) =>{
+        try {
+            const user = jwt.verify(token, JWT_SECRET);
+            return user;       
+        } catch (error) {
+            console.log({error});
+            return null;
+        }
     }
-}
