ef5f7da598c7b5dcb4ff674c2c775730197493d4
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-04-17 19:34:06 -0400

    adding beufy

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Buefy from "buefy"
 import App from './App.vue'
 import router from './router'
 
@@ -7,6 +8,8 @@ import 'buefy/dist/buefy.css'
 import '@fortawesome/fontawesome-free/css/all.css';
 import './sass/main.scss'
 
+Vue.use(Buefy);
+Vue.component('FriendsShort', FriendsShort);
 Vue.config.productionTip = false
 
 new Vue({
---
+++ client/src/models/Session.js
@@ -0,0 +1,26 @@
+
+
+
+import { ToastProgrammatic as toastr } from "buefy";
+
+const Session = {
+    user: null,
+    message: []
+}
+
+export default Session;
+
+export function Login() {
+    Session.user = { name: 'Jasmine', handle: '@jasmineennabe' }
+}
+export function Logout() {
+    Session.user = null
+}
+
+export function toastError(msg){
+    toastr.open({
+        message: msg,
+        queue: false,
+        type: 'is-danger'
+    })
+} 
