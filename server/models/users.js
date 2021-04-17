8b340e3c4efd6975cfb12b2bdddd35aec9525884
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-04-17 16:35:48 -0400

    update users c&m

--- server/models/users.js
+++ server/models/users.js
@@ -33,6 +33,7 @@ const list = [
 
 module.exports.GetAll = ()=> list;
 module.exports.Get = (user_id)=> list[user_id];
+module.exports.GetByHandle = (handle)=> ({ ...list.find( (x, i)=> x.handle == handle ), password: undefined }) ;
 module.exports.Add = (user)=> {
     if(!user.firstName){
         throw "First Name is reqired"
