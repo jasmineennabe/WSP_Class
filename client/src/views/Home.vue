5386eb25fbc7a8d63b19080e966f3dcada35cf99
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-04-17 19:10:05 -0400

    syncing client & server

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,21 +2,17 @@
 
     <div class="home">
         <h1>This is the home page</h1>
-
-
-
-
-
-
-
-
-
-
     </div>
 
 </template>
 
 <script>
+    import { api } from "../models/myFetch";
+
+    ( async ()=>{
+        const actualItem = await api("posts");
+        console.log({actualItem});
+    })()
 
 export default {
   name: 'Home',
